0af97df8a79e8a389b750dc8f2c1580ef197ac81
happiness_m <2789140037@qq.com>
2020-04-29 11:50:45 +0800

    职称管理添加swagger

--- src/main/java/com/niit/vhr/controller/system/basic/JobLevelController.java
+++ src/main/java/com/niit/vhr/controller/system/basic/JobLevelController.java
@@ -3,6 +3,8 @@ package com.niit.vhr.controller.system.basic;
 import com.niit.vhr.model.JobLevel;
 import com.niit.vhr.model.RespBean;
 import com.niit.vhr.service.system.basic.JobLevelService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,16 +12,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/system/basic/job")
+@Api(value = "JobLevelController", tags = {"职称管理"})
 public class JobLevelController {
     @Autowired
     JobLevelService jobLevelService;
     @GetMapping("/")
+    @ApiOperation(value = "获取职称",notes = "获取数据库里的所有职称")
     public RespBean getAllJobLevel() {
         List<JobLevel> jobLevels = jobLevelService.getAllJobLevel();
         return RespBean.ok("获取成功", jobLevels);
     }
 
     @PostMapping("/")
+    @ApiOperation(value = "添加职称",notes = "添加用户输入的新的职称")
     public RespBean addJobLevel(@RequestBody JobLevel jobLevel) {
         if(jobLevelService.addJobLevel(jobLevel) == 1 ) {
             return RespBean.ok("添加成功");
@@ -28,6 +33,7 @@ public class JobLevelController {
     }
 
     @PutMapping("/")
+    @ApiOperation(value = "修改职称",notes = "根据传入的职称信息修改职位")
     public RespBean updateJobLevel(@RequestBody JobLevel jobLevel) {
         if(jobLevelService.updateJobLevel(jobLevel)== 1 ) {
             return RespBean.ok("更新成功");
@@ -36,6 +42,7 @@ public class JobLevelController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除职称",notes = "根据id删除职称")
     public RespBean deleteJobLevel(@PathVariable Integer id) {
         if(jobLevelService.deleteJobLevel(id) == 1 ) {
             return RespBean.ok("删除成功");
@@ -44,6 +51,7 @@ public class JobLevelController {
     }
 
     @DeleteMapping("/")
+    @ApiOperation(value = "批量删除职称",notes = "根据ids 批量删除职称")
     public RespBean deleteJobLevel(Integer[] ids) {
         if(jobLevelService.deleteJobLevel(ids) == ids.length) {
             return RespBean.ok("批量删除成功");
--- src/main/java/com/niit/vhr/model/JobLevel.java
+++ src/main/java/com/niit/vhr/model/JobLevel.java
@@ -8,14 +8,14 @@ import java.util.Date;
 @Data
 public class JobLevel {
     private Integer id;
-
+//职称名称
     private String name;
-
+//职称等级
     private String titleLevel;
-
+//创建职称的时间
     @JsonFormat(pattern = "yyyy-MM-dd hh:hh", timezone = "Asia/Shanghai")
     private Date createDate;
-
+//是否启用
     private Boolean enabled;
 
 
